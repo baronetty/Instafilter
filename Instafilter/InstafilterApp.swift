6dfb55270cf60c2ce7a5c908843677e9ba49bdf6
Leo Barnett <leo.e.barnett@icloud.com>
2023-12-16 10:21:47 +0200

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Leo  on 16.12.23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
