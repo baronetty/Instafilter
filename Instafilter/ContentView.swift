42a58d334e654ef4ec72755e63dc4d693650b461
Leo Barnett <leo.e.barnett@icloud.com>
2023-12-17 17:16:09 +0200

    Version 1.0

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -4,29 +4,51 @@
 //
 //  Created by Leo  on 16.12.23.
 //
-
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
     
     var body: some View {
-        Button("Hello, world!") {
-            showingConfirmation.toggle()
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
         }
-        .buttonStyle(.borderedProminent)
-        .foregroundStyle(.white)
-        .frame(width: 300, height: 300)
-        .background(backgroundColor)
-        .confirmationDialog("Change Background", isPresented: $showingConfirmation) {
-            Button("Red") { backgroundColor = .red }
-            Button("Green") { backgroundColor = .green }
-            Button("Blue") { backgroundColor = .blue }
-            Button("Cancel", role: .cancel) { }
-        } message: {
-            Text("Select a new color")
-        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        let inputImage = UIImage(resource: .example)
+        let beginImage = CIImage(image: inputImage)
+
+        let context = CIContext()
+        let currentFilter = CIFilter.pixellate()
+        
+        currentFilter.inputImage = beginImage
+
+        let amount = 1.0
+
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
+        
+        // get a CIImage from our filter or exit if that fails
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        // attempt to get a CGImage from our CIImage
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
+        // convert that to a UIImage
+        let uiImage = UIImage(cgImage: cgImage)
+
+        // and convert that to a SwiftUI image
+        image = Image(uiImage: uiImage)
     }
 }
 
